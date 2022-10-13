a7a4cd19a78c0ef8670c0320b505af389e22afa9
asurowy <101614024+asurowy@users.noreply.github.com>
2022-10-13 15:05:41 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault()
+      buildList(e.target['new-task-description'].value)
+  })
 });
+
+function buildList(thingsToDo){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', timeToDelete)
+    btn.textContent = "x"
+    p.textContent = thingsToDo
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector('#list').appendChild(p)
+}
+
+function timeToDelete(e){
+  e.target.parentNode.remove()
+}
